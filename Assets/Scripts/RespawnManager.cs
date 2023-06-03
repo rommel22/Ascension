9a7cba2e437c0af9b087907f59395ef34a14f020
AncuL001 <ancul001@gmail.com>
2023-06-04 00:46:40 +0700

    implement checkpoint notification on level 3

--- Assets/Scripts/RespawnManager.cs
+++ Assets/Scripts/RespawnManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class RespawnManager : MonoBehaviour
 {
     public GameObject respawnPointAfterCheckpoint;
+    public RectTransform checkpointNotification;
+    public RectTransform checkpointDisplayPosition;
     bool hasReachedCheckpoint;
     void Start()
     {
@@ -18,5 +20,24 @@ public class RespawnManager : MonoBehaviour
         hasReachedCheckpoint = true;
         FindObjectOfType<HealthManager>().setRespawnPoint(respawnPointAfterCheckpoint);
         FindObjectOfType<HealthManager>().currentHealth = FindObjectOfType<HealthManager>().maxHealth;
+
+        StartCoroutine(MoveCheckpointNotification());
+    }
+
+    public IEnumerator MoveCheckpointNotification()
+    {
+        Vector3 originalPosition = checkpointNotification.transform.position;
+        float step = 0.5f * Time.fixedDeltaTime;
+
+        for (float t = 0; t <= 1.0f; t += step) {
+            checkpointNotification.transform.position = Vector3.Lerp(checkpointNotification.transform.position, checkpointDisplayPosition.transform.position, t);
+            yield return new WaitForEndOfFrame();
+        }
+        yield return new WaitForSeconds(3);
+
+        for (float t = 0; t <= 1.0f; t += step) {
+            checkpointNotification.transform.position = Vector3.Lerp(checkpointNotification.transform.position, originalPosition, t);
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
